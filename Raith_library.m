c135fa508d85f040640634207e61415bbf8c5713
ahryciw <aaron.hryciw@nrc-cnrc.gc.ca>
2014-09-19 16:20:55 -0600

    Fixed 'aref' plotting bug

--- Raith_library.m
+++ Raith_library.m
@@ -394,12 +394,13 @@ classdef Raith_library < handle
                         V=reshape(V,1,prod(ELE.data.n_colrow));
                         
                         UV=M*obj.trans([ELE.data.uv_0(1) ELE.data.uv_0(2)])*obj.rot(ELE.data.angle)*[U;V;ones(size(U))];  % Transformations applied to aref object (if coming from a parent sref or aref element)
-                        M(7:8)=0;  % Remove translation component
+                        M2=M;
+                        M2(7:8)=0;  % Remove translation component
                         
                         for k=1:length(U)
-                            Mk=obj.trans(UV(:,k))*M*obj.rot(ELE.data.angle)*obj.scale(ELE.data.mag)*obj.refl(ELE.data.reflect);    
+                            Mk=obj.trans(UV(:,k))*M2*obj.rot(ELE.data.angle)*obj.scale(ELE.data.mag)*obj.refl(ELE.data.reflect);    
                             if ~any(strcmp(obj.structlist,ELE.data.name))
-                                ELE.plot(M,scDF);
+                                ELE.plot(M2,scDF);
                             else
                                 obj.plot(ELE.data.name,Mk,scDF);
                             end
